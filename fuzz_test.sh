01125f142afd76162f67bf5678c1b6d13f5481fc
MJ-SEO <21600343@handong.edu>
2022-01-19 14:23:57 +0900

    AFL test

--- fuzz_test.sh
+++ fuzz_test.sh
@@ -7,8 +7,7 @@ cd ..
 mkdir seeds
 echo input >> seeds/inp1 
 AFLplusplus/afl-gcc -o fuzz test_fuzzing.c
-
 if [ -z "$AFL_STOP_MANUALLY" ];
 then AFLplusplus/afl-fuzz -i seeds -o out ./fuzz
-else timeout --preserve-status 5s ./afl-fuzz -i seeds -o out ./fuzz
+else timeout --preserve-status 10s AFLplusplus/afl-fuzz -i seeds -o out ./fuzz
 fi
